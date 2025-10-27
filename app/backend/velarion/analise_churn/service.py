00df76beefb8d602c92b80e3c445f7f975974426
carlos alecrim <csilva1073@gmail.com>
2025-10-26 22:58:29 -0400

    respuesta en probabilidad del modelo Lightgbm

--- app/backend/velarion/analise_churn/service.py
+++ app/backend/velarion/analise_churn/service.py
@@ -6,6 +6,6 @@ def calcular_churn_probabilidad(data):
     X_proc = pipeline.transform(data)
 
     model = get_model()
-    prob = float(model.predict(X_proc).flatten()[0])
-
-    return prob
+    prob = float(model.predict_proba(X_proc)[:, 1][0])
+    print(prob)
+    return f"{prob:.3f}"
