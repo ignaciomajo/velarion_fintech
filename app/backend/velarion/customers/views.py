fcc9d590eb4e78b026dbe1c2e7ebf453f8ec0290
carlos alecrim <csilva1073@gmail.com>
2025-10-26 22:42:12 -0400

    metodo resumen riesgo

--- app/backend/velarion/customers/views.py
+++ app/backend/velarion/customers/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Count
 from rest_framework.pagination import PageNumberPagination
 from .models import ClientFeature, ChurnPrediction
 from .serializers import ClientFeatureSerializer, ChurnPredictionSerializer
@@ -25,4 +28,14 @@ class ChurnPredictionViewSet(viewsets.ModelViewSet):
     queryset = ChurnPrediction.objects.all()
     serializer_class = ChurnPredictionSerializer
     pagination_class = ChurnPredictionPagination
+
+    @action(detail=False, methods=['get'])
+    def resumen_riesgo(self, request):
+        data = (
+            ChurnPrediction.objects
+            .values('riesgo')
+            .annotate(total=Count('riesgo'))
+            .order_by('-total')
+        )
+        return Response(list(data))
 
