f763c8fbb71a0b18b3b5a34751d3c80ad41a2198
carlos alecrim <csilva1073@gmail.com>
2025-10-30 21:25:28 -0400

    configuracion de variable de entorno

--- app/frontend/mkapp/vite.config.js
+++ app/frontend/mkapp/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) =>{
+  const env = loadEnv(mode,process.cwd());
+
+  const API_URL = `${env.VITE_BACKEND_URL}`;
+  return {
   plugins: [react()],
-})
+  server:{
+    proxy:{
+      '/api':{
+        target: API_URL,
+        changeOrigin:true,
+        secure:false
+      }
+    }
+  }
+};
+});
