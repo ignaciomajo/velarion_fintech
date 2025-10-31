69b9cf335950c105f9df8c1102cb1ddab8fda46d
carlos alecrim <csilva1073@gmail.com>
2025-10-30 22:20:33 -0400

    permission 2

--- app/backend/velarion/velarion/settings.py
+++ app/backend/velarion/velarion/settings.py
@@ -32,9 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
-CORS_ALLOWED_ORIGINS = [
-    "*",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -85,6 +83,7 @@ CORS_ALLOW_METHODS = [
     'POST',
     'PUT',
 ]
+
 
 CORS_ALLOW_HEADERS = [
     'accept',
